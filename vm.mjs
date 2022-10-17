ed74c9e2b0f6d8924f589f3feb9cf9c7027079db
fenya <dim74.09@mail.ru>
2022-10-17 22:11:24 +0500

    remove unused files

--- vm.mjs
+++ vm.mjs
@@ -2,9 +2,9 @@ import Memory from "./Memory.mjs";
 import Processor from "./Processor.mjs";
 import fs from 'fs'
 const stackSize = 50;
-let program = fs.readFileSync(process.argv[2]).toString().replace(/;.*\n/g, ''); //delete comments
-program = program.replace(/\n+/g, '\n');
-program = program.split(/[\s \n]/g); // split for write to memory
+let program = fs.readFileSync(process.argv[2]).toString().replace(/;.*\n/g, ''); //delete comments.
+program = program.replace(/\n+/g, '\n'); //delete \n repeats.
+program = program.split(/[\s \n]/g); // split for write to memory.
 let ram = new Memory(program.length + stackSize);
 for(let i = stackSize; i < stackSize + program.length; i++) {
     ram.write(i, program[i-stackSize]);
